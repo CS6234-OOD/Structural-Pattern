e70d418a9af2b0a8daca027f3f0c0936992cb817
Oak-Leaf <kushnery@gwu.edu>
2017-03-20 15:48:05 -0400

    Update administrator.java

--- administrator.java
+++ administrator.java
@@ -1,21 +1,27 @@
 
-public class administrator implements accountPriv {
+import java.util.Random;
 
+public class administrator implements accountPriv {
+    Random generator = new Random();
 
     @Override
-    public void Registration() {
-        System.out.print("\n Admin closed registration");
+    public void Registration(int courseNumber) {
+        System.out.print("\n Admin closed registration for course" + courseNumber);
+        
     }
 
     @Override
     public void deleteCourse() {
-        System.out.print("\n Admin deleted course");
+        int confirmationNumber = generator.nextInt(10) + 1;
+        System.out.print("\n Admin deleted course, confirmation number for transaction is: " + confirmationNumber);
     }
 
     @Override
-    public void updateCourse() {
-        System.out.print("\n Admin updated the course atributes");
+    public void updateCourse(String courseName) {
+        System.out.print("\n Admin updated the course atributes for " + courseName);
+        
     }
 
     
 }
+
