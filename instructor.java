d28f3ac9366ae859317e77f81a440804af78a1ad
Oak-Leaf <kushnery@gwu.edu>
2017-03-20 15:48:26 -0400

    Update instructor.java

--- instructor.java
+++ instructor.java
@@ -1,16 +1,21 @@
 
+
 public class instructor  {
 
-    public void cancelCourse() {
-        System.out.print("\n Instructor canceled course for today only");
-    }
-    
-    public void uploadMaterial() {
-        System.out.print("\n Instructor uploaded course material");
+    public void cancelCourse(int courseNumber) {
+        System.out.print("\n Instructor canceled course number " + courseNumber + "for today only");
+        // return 
     }
     
     public void reassignCourse() {
         System.out.print("\n Instructor changed");
     }
     
+    public void uploadMaterial(String courseName) {
+        System.out.print("\n Instructor uploaded course material for " +  courseName);
+    }
+    
+
+    
 }
+
